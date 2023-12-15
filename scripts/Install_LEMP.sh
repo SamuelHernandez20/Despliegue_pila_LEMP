a115c1d5e6ce3e815b88caf9c9fde12a5fb6f713
samuel <samuel@gmail.com>
2023-12-15 17:21:46 +0000

    "Instalación de la pila LEMP"

---
+++ scripts/Install_LEMP.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Actualizamos los repos
+
+apt update
+
+# Actualizar paquetes 
+
+#apt upgrade
+
+#Instalar el servidor web Nginx:
+
+apt install nginx -y
+
+# Instalación de php-fpm y php-mysql:
+#-----------------------------------------------
+apt install php-fpm php-mysql -y
+#-----------------------------------------------
+
+#Copiar el archivo de conf de Nginx:
+
+cp ../conf/default /etc/nginx/sites-available/default
+
+#Reiniciar servicio Nginx:
+
+systemctl restart nginx
+
+# copiar archivo php:
+
+cp ../php/info.php /var/www/html
+
+# modificar el propietario y grupo de /var/www/html para usuario de Nginx:
+
+chown -R www-data:www-data /var/www/html
+
+
